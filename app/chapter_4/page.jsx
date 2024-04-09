b980178109bd6cba6ebad8dcd81779a14f0350d5
azizchyayan123 <aaziz9642@gmail.com>
2024-04-09 12:56:54 +0600

    added

--- app/chapter_4/page.jsx
+++ app/chapter_4/page.jsx
@@ -2,7 +2,8 @@
 import QuizComponent from '../../components/QuizComponent';
 import banglaData from '../../data/chapter_4/banglaData';
 import hindiData from '../../data/chapter_4/hindiData';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'; // Correct import
+import { useSearchParams } from 'react-router-dom'; // Correct import
+import { Suspense } from 'react';
 
 // Import the CSS module at the top of your file
 import styles from './Chapter_4.module.css';
@@ -12,23 +13,26 @@ const Chapter_4 = () => {
   const searchParams = useSearchParams();
   const language = searchParams.get('language');
 
-  return (<>
-    <Navbar />
-    <div className={styles.container}>
-      <h1 className={styles.title}>Chapter 8</h1>
-      {language === "bangla" ? (
-        <div className={styles.quizWrapper}>
-          <QuizComponent quizData={banglaData} />
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+        <div className={styles.container}>
+          <h1 className={styles.title}>Chapter 8</h1>
+          {language === "bangla" ? (
+            <div className={styles.quizWrapper}>
+              <QuizComponent quizData={banglaData} />
+            </div>
+          ) : language === "hindi" ? (
+            <div className={styles.quizWrapper}>
+              <QuizComponent quizData={hindiData} />
+            </div>
+          ) : (
+            <p className={styles.errorMessage}>Content not found..!</p>
+          )}
         </div>
-      ) : language === "hindi" ? (
-        <div className={styles.quizWrapper}>
-          <QuizComponent quizData={hindiData} />
-        </div>
-      ) : (
-        <p className={styles.errorMessage}>Content not found..!</p>
-      )}
-    </div>
-  </>
+      </Suspense>
+    </>
   );
 };
 
