caa65afbc03239c87d0578c09317cab5d60b381b
azizchyayan123 <aaziz9642@gmail.com>
2024-04-23 11:17:52 +0600

    added

--- app/_documents.js
+++ app/_documents.js
@@ -1,2 +1,4 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3158034660522527"
+<Head>
+   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3158034660522527"
      crossorigin="anonymous"></script>
+</Head>
